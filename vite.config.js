657f0aec516fa9be23d21de28be0c88160d928d9
Semet <hamdanilombok@gmail.com>
2023-02-20 08:41:19 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    esbuild: {
+        loader: "jsx",
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                ".js": "jsx",
+            },
+        },
+    },
+    plugins: [
+        react(),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
